85b42649f43e813731eeaf43e11ac1affbef48f8
reinfajar <tsanimfajar@outlook.com>
2020-07-30 17:51:05 +0700

    done

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,74 @@
+<template>
+  <section class="text-gray-700 body-font relative">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-12">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Play With Word</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Just enter the word and it will change for you</p>
+    </div>
+    <div class="lg:w-1/2 md:w-2/3 mx-auto">
+      <div class="flex flex-wrap -m-2">
+        <div class="p-2 w-1/2">
+          <input 
+          class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" 
+          placeholder="Insert Your Word" 
+          type="text"
+          v-model="word">
+        </div>
+        <div class="p-2 w-full">
+          <button 
+          class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          @click.prevent="capitalize"
+          >
+          Capitalize
+          </button>
+          <button 
+          class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          @click.prevent="randomize"
+          >
+          Randomize
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col text-center w-full mb-12" v-if="changedWord !== ''">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">{{ changedWord }}</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">{{ word }}</p>
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      word: '',
+      changedWord: ''
+    }
+  },
+  methods: {
+    capitalize () {
+      this.changedWord = ''
+      this.changedWord = String(this.word).toUpperCase()
+    },
+    randomize () {
+      this.changedWord = ''
+      const word = this.word.split('')
+      word.forEach(el => {
+        const random = Math.ceil(Math.random() * 10)
+        if (random > 5) {
+          el = el.toUpperCase()
+        } else if (random < 5) {
+          el = el.toLowerCase()
+        }
+        this.changedWord += el
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ tests/unit/test.spec.js
@@ -0,0 +1,29 @@
+import { shallowMount } from "@vue/test-utils";
+import Home from '../../src/components/HelloWorld.vue'
+const { name } = Home;
+
+describe("Home", () => {
+  let index;
+  beforeEach(() => {
+    index = shallowMount(Home);
+  });
+
+  it("have name", () => {
+    expect(name).toBe("HelloWorld");
+  });
+
+  it("renders form input", () => {
+    expect(index.html()).toContain(`input`);
+    expect(index.html()).toContain('button');
+  });
+
+  it("has script", () => {
+    expect(typeof Home.data).toBe(`function`);
+    expect(typeof Home.methods).toBe(`object`);
+  });
+
+  it("return", () => {
+    expect(Home.data().word).toBe('');
+    expect(Home.data().changedWord).toBe('');
+  });
+});
